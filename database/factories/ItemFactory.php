fff2e8e15b2766f1198895c55275f3029e58162a
Bas Racquey Biensika <basracqueyug@gmail.com>
2025-10-03 03:53:30 +0700

    Add models, migrations, and seeders for User, Role, Category, Item, Order, and OrderItem

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+class ItemFactory extends Factory
+{
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'category_id' => $this->faker->numberBetween(1, 2),
+            'price' => $this->faker->randomFloat(2, 10000, 100000),
+            'description' => $this->faker->text(),
+            'image' => $this->faker->imageUrl(),
+            'is_active' => $this->faker->boolean(),
+        ];
+    }
+}
