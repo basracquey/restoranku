606641e2278c104bef59d1c96cf5c6437fb045df
Bas Racquey Biensika <basracqueyug@gmail.com>
2025-10-12 18:25:26 +0700

    Update ItemFactory to use predefined image URLs and modify DatabaseSeeder to call ItemSeeder

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -15,7 +15,13 @@ class ItemFactory extends Factory
             'category_id' => $this->faker->numberBetween(1, 2),
             'price' => $this->faker->randomFloat(2, 10000, 100000),
             'description' => $this->faker->text(),
-            'image' => $this->faker->imageUrl(),
+            'img' => fake()->randomElement(
+                [
+                'https://images.unsplash.com/photo-1591325418441-ff678baf78ef',
+                'https://plus.unsplash.com/premium_photo-1668143358351-b20146dbcc02',
+                'https://images.unsplash.com/photo-1742633882704-41ec3a57dbb7'
+                ]
+        ),
             'is_active' => $this->faker->boolean(),
         ];
     }
