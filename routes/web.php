9a18abdfc57d0fb9a7645048674aee649bc090c1
Bas Racquey Biensika <basracqueyug@gmail.com>
2025-10-12 18:27:58 +0700

    Add MenuController and views for menu, cart, and checkout functionality

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\MenuController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('menu');
 });
+
+Route::get('/menu', [MenuController::class, 'index'])->name('menu');
+
+Route::get('/cart', [MenuController::class, 'cart'])->name('cart');
+Route::post('/cart/add', [MenuController::class, 'addToCart'])->name('cart.add');
+// Route::post('/cart/update', [App\Http\Controllers\CartController::class, 'update'])->name('cart.update');//sementara
+// Route::post('/cart/remove', [App\Http\Controllers\CartController::class, 'remove'])->name('cart.remove');//sementara
+
+Route::get('/checkout', function () {
+    return view('customer.cart');
+})->name('checkout');
